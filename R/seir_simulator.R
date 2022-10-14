da676f6b678999f72b1127896f5acf7238fd66cd
stefanradev93 <stefan.radev93@gmail.com>
2022-10-14 09:01:03 +0200

    Add seir_simulator.R and sir_simulator.R to R directory

---
+++ R/seir_simulator.R
@@ -0,0 +1,45 @@
+#' Simulate from a basic discrete-time SIR model
+#'
+#' @param beta infection rate
+#' @param eta rate E -> I
+#' @param gamma recovery rate
+#' @param N population size
+#' @param T_steps number of time steps to take
+#' @param E0 number of initially exposed
+#'
+#' @return I(t) - vector of infected inviduals over T_steps
+#' @export
+SEIR = function(beta, eta, gamma, N, T_steps, E0=1) {
+  
+  # Prepare vectors
+  S = rep(NA, T_steps)
+  E = rep(NA, T_steps)
+  I = rep(NA, T_steps)
+  R = rep(NA, T_steps)
+  
+  # Initial conditions
+  S[1] = N - E0
+  E[1] = E0
+  I[1] = 0
+  R[1] = 0
+  
+  # Run pandemic in discrete steps
+  for (t in 2:T_steps) {
+    
+    # Newly exposed
+    E_new = beta*I[t-1]*S[t-1] / N
+    
+    # Newly infected
+    I_new = eta*E[t-1]
+    
+    # Newly recovered
+    R_new = gamma*I[t-1]
+    
+    # Update compartments
+    S[t] = S[t-1] - E_new
+    E[t] = E[t-1] + E_new - I_new
+    I[t] = I[t-1] + I_new - R_new
+    R[t] = R[t-1] + R_new
+  }
+  return(I)
+}
---
+++ R/sir_simulator.R
@@ -0,0 +1,38 @@
+#' Simulate from a basic discrete-time SIR model
+#'
+#' @param beta infection rate
+#' @param gamma recovery rate
+#' @param N population size
+#' @param T_steps number of time steps to take
+#' @param I0 number of initially infected
+#'
+#' @return I(t) - vector of infected inviduals over T_steps
+#' @export
+SIR = function(beta, gamma, N, T_steps, I0=1) {
+  
+  # Prepare vectors
+  S = rep(NA, T_steps)
+  I = rep(NA, T_steps)
+  R = rep(NA, T_steps)
+  
+  # Initial conditions
+  S[1] = N - I0
+  I[1] = I0
+  R[1] = 0
+  
+  # Run pandemic in discrete steps
+  for (t in 2:T_steps) {
+    
+    # Newly infected
+    I_new = beta*I[t-1]*S[t-1] / N
+    
+    # Newly recovered
+    R_new = gamma*I[t-1]
+    
+    # Update compartments
+    S[t] = S[t-1] - I_new
+    I[t] = I[t-1] + I_new - R_new
+    R[t] = R[t-1] + R_new
+  }
+  return(I)
+}
